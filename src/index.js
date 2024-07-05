c2f50e4b6d6488bba62697a85c1e64aef3cfc145
SteveLydon <156434689+SteveLydon@users.noreply.github.com>
2024-07-05 13:26:33 +0100

    First draft of project template

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+ function component() {
+    const element = document.createElement('div');
+    return element;
+ }
+ document.body.classList.add("reset");
+ document.body.appendChild(component());
